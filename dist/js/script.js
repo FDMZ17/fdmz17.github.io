117f1a3ea7d892d2564fae5853ac02d5be77447f
FDMZ17 <devoloper.fdmz17@gmail.com>
2022-05-16 11:01:40 +0700

    Mon May 16 11:01:40 AM WIB 2022

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,20 @@ hamburger.addEventListener("click", function() {
   hamburger.classList.toggle("hamburger-active")
   navMenu.classList.toggle("hidden");
 })
+
+// Contact form!
+const scriptURL = "https://script.google.com/macros/s/AKfycbyBPkZ5NiKPp-8YbKvJgF5ZOS69NrUCUFDiUMnyfwGFEqrpar99rbC3nmOq1TeXRDZX/exec";
+const form = document.forms["contact-form"];
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, {
+      method: "POST",
+      body: new FormData(form)
+    })
+    .then((response) => {
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
