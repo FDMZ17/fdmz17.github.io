c7d04cee112659571fc7868fc7ab12ec3bafa27c
FDMZ17 <devoloper.fdmz17@gmail.com>
2022-05-23 14:57:53 +0700

    Mon May 23 02:57:52 PM WIB 2022

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["index.html", "blog/index.html", "dist/js/script.js"],
+  content: ["index.html", "dist/js/script.js"],
   darkMode:  "class",
   theme: {
     container: {
